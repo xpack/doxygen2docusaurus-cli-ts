d59014729dc3f0505d69fc020e4d6bcdce10457d
Liviu Ionescu <ilg@livius.net>
2025-04-10 01:38:52 +0300

    move formatDate to utils

--- src/docusaurus-generator/index.ts
+++ src/docusaurus-generator/index.ts
@@ -29,7 +29,7 @@ import { Namespaces } from './data-model/namespace.js'
 import { Classes } from './data-model/classes.js'
 import { GroupGenerator } from './generators/group.js'
 import { FrontMatter } from './types.js'
-import { KindGeneratorBase as GeneratorBase } from './generators/generator-base.js'
+import { formatDate } from './utils.js'
 
 // ----------------------------------------------------------------------------
 
@@ -340,7 +340,7 @@ export class DocusaurusGenerator {
       }
     }
     frontMatterText += '\n'
-    frontMatterText += `date: ${this.formatDate(new Date())}\n`
+    frontMatterText += `date: ${formatDate(new Date())}\n`
     frontMatterText += '\n'
     frontMatterText += '---\n'
     frontMatterText += '\n'
@@ -353,17 +353,6 @@ export class DocusaurusGenerator {
     await fileHandle.close()
   }
 
-  formatDate (date: Date): string {
-    // Custom format: YYYY-MM-DD HH:mm:ss
-    const year = date.getUTCFullYear()
-    const month = String(date.getUTCMonth() + 1).padStart(2, '0') // Months are zero-based
-    const day = String(date.getUTCDate()).padStart(2, '0')
-    const hours = String(date.getUTCHours()).padStart(2, '0')
-    const minutes = String(date.getUTCMinutes()).padStart(2, '0')
-    const seconds = String(date.getUTCSeconds()).padStart(2, '0')
-
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} +0000`
-  }
 }
 
 // ----------------------------------------------------------------------------
---
+++ src/docusaurus-generator/utils.ts
@@ -0,0 +1,26 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2025 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT.
+ */
+
+// ----------------------------------------------------------------------------
+
+export function formatDate (date: Date): string {
+  // Custom format: YYYY-MM-DD HH:mm:ss
+  const year = date.getUTCFullYear()
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0') // Months are zero-based
+  const day = String(date.getUTCDate()).padStart(2, '0')
+  const hours = String(date.getUTCHours()).padStart(2, '0')
+  const minutes = String(date.getUTCMinutes()).padStart(2, '0')
+  const seconds = String(date.getUTCSeconds()).padStart(2, '0')
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} +0000`
+}
+
+// ----------------------------------------------------------------------------
