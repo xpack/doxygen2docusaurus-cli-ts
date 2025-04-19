20311771c84d81d5a9c91b7b80a3501c12a90556
Liviu Ionescu <ilg@livius.net>
2025-04-19 20:37:21 +0300

    add IncludesList* components

---
+++ src/components/IncludesListItem/index.js
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2024 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT.
+ */
+
+// ----------------------------------------------------------------------------
+
+import styles from './styles.module.css';
+import React from 'react';
+import Link from '@docusaurus/Link'
+
+// ----------------------------------------------------------------------------
+
+export default function IncludesListItem({
+  filePath,
+  permalink,
+  isLocal,
+  children
+}) {
+  return (
+    <p class="doxyInclude">
+      <code>#include {isLocal === 'true' ? '"' : '<'}<Link to={permalink}>{filePath}</Link>{isLocal === 'true'  ? '"' : '>'}</code>
+    </p>
+  );
+}
+
+// ----------------------------------------------------------------------------
