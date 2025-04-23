9af513c5a731a1586ad898cc895b650f4727dd5c
Liviu Ionescu <ilg@livius.net>
2025-04-23 20:55:02 +0300

    package.json move exports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,19 @@
 
 export { default } from './plugin/docusaurus.js'
 
+export { default as CodeLine } from '../components/CodeLine/index.js'
+export { default as DoxygenPage } from '../components/DoxygenPage/index.js'
+export { default as Highlight } from '../components/Highlight/index.js'
+export { default as IncludesList } from '../components/IncludesList/index.js'
+export { default as IncludesListItem } from '../components/IncludesListItem/index.js'
+export { default as MemberDefinition } from '../components/MemberDefinition/index.js'
+export { default as MembersList } from '../components/MembersList/index.js'
+export { default as MembersListItem } from '../components/MembersListItem/index.js'
+export { default as ParametersList } from '../components/ParametersList/index.js'
+export { default as ProgramListing } from '../components/ProgramListing/index.js'
+export { default as SectionDefinition } from '../components/SectionDefinition/index.js'
+export { default as SectionUser } from '../components/SectionUser/index.js'
+export { default as TreeTable } from '../components/TreeTable/index.js'
+export { default as TreeTableRow } from '../components/TreeTableRow/index.js'
+
 // ----------------------------------------------------------------------------
