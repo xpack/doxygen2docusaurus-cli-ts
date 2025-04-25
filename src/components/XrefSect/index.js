ad1dfc2acbde8f960803ae1b4f8eef489a12af4c
Liviu Ionescu <ilg@livius.net>
2025-04-26 01:19:15 +0300

    add XrefSect

---
+++ src/components/XrefSect/index.js
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2024 Liviu Ionescu. All rights reserved.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose is hereby granted, under the terms of the MIT license.
+ *
+ * If a copy of the license was not distributed with this file, it can
+ * be obtained from https://opensource.org/licenses/MIT.
+ */
+
+// ----------------------------------------------------------------------------
+
+import styles from './styles.module.css';
+import React from 'react';
+
+import Link from '@docusaurus/Link'
+
+// ----------------------------------------------------------------------------
+
+export default function XrefSect({title, permalink, children}) {
+  return (
+    <div class="doxyXrefSect">
+      <dl>
+        <dt>
+          <b><Link to={permalink}>{title}</Link></b>
+        </dt>
+      </dl>
+      <dd>
+        {children}
+      </dd>
+    </div>
+  );
+}
+
+// ----------------------------------------------------------------------------
