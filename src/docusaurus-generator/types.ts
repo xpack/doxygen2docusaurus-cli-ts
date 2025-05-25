f9c00390aa8251029cdb16656e2bf1acd4ebbf54
Liviu Ionescu <ilg@livius.net>
2025-05-25 09:56:17 +0300

    types.ts add collapsibleTableRow

--- src/docusaurus-generator/types.ts
+++ src/docusaurus-generator/types.ts
@@ -17,4 +17,14 @@ export interface FrontMatter {
   [key: string]: string | string[] | null | boolean | number
 }
 
+export interface collapsibleTableRow {
+  id: string
+  iconLetter?: string
+  iconClass?: string
+  label: string
+  link: string
+  description: string
+  children?: collapsibleTableRow[]
+}
+
 // ----------------------------------------------------------------------------
