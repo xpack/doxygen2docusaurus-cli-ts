ae6c4bbc6c86a8cdb76938c0d15d4a3ba978830e
Liviu Ionescu <ilg@livius.net>
2025-07-15 10:37:55 +0300

    workspace.ts cosmetics

--- src/docusaurus/workspace.ts
+++ src/docusaurus/workspace.ts
@@ -204,7 +204,7 @@ export class Workspace extends Renderers {
 
     // doxygen2docusaurus
     this.projectPath = path.dirname(path.dirname(__dirname))
-    console.log(__dirname, this.projectPath)
+    // console.log(__dirname, this.projectPath)
 
     this.dataModel = dataModel
     this.options = options
